732ebb1fe3a98904404363589728c8d6d03273be
GIA PHAM <giapham121994@gmail.com>
2024-07-18 22:23:30 +0700

    aa

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'dbwp' );
+define( 'DB_NAME', 'chi05601_dbwp' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'chi05601_giapham' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '27121994aA!!' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
